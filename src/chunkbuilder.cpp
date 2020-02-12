cfe6acd700036c81675de6eb70da87732182d3c8
FSchubert <frederick.schubert@t-online.de>
2020-02-12 23:53:14 +0100

    update to godot 3.2

--- src/chunkbuilder.cpp
+++ src/chunkbuilder.cpp
@@ -108,7 +108,7 @@ void ChunkBuilder::buildChunk(std::shared_ptr<Chunk> chunk, Node* game) {
 			meshes.push_back(meshData);
 		}
 
-		Variant v = game->call_deferred("build_chunk", meshes, Ref<Chunk>(chunk.get()));
+		game->call_deferred("build_chunk", meshes, Ref<Chunk>(chunk.get()));
 	}
 	catch (const std::exception & e) {
 		std::cerr << boost::diagnostic_information(e);
